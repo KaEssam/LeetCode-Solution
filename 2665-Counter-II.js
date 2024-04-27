f49b0f49650dffadcc2d9b56fe9268f3dcf79ef7
Karim Essam <126638336+KaEssam@users.noreply.github.com>
2024-04-27 13:05:15 +0300

    [2665] [Time Beats: 8.73%] [Memory Beats: 83.62%] - LeetPush

---
+++ 2665-Counter-II.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let n = init;
+    return{
+        increment(){
+            return ++n
+        },
+        decrement(){
+            return --n
+        },
+        reset(){
+            return n=init
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
